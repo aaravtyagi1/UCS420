933f702cf97f58be8f7f11714064ad401d1d2c34
aaravtyagi1 <aaravtyagi833@gmail.com>
2026-07-28 08:46:01 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+
+print("Hello World")
+print("Aarav Tyagi")
+print("Aarav Tyagi")
+print("Aarav Tyagi")
+i = 5
+j = 10 
+print(i+j)
+
+x =  'abc'
+y = 'def'
+w='ghi'
+z=  x+y
+print(z)
+
+x1 =  'abc'
+y1 = 3
+z1=  x1+ str(y1)
+print(z1)
+
+k = 15
+l = i+j+k
+print(l)
+
+o = x+y+w
+print(o)
